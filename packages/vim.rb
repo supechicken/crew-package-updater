e2890c4934838fdda3971e4d65f2bc8e75a74a98
supechicken <supechicken666@gmail.com>
2022-02-12 02:32:49 +0800

    Create vim.rb

---
+++ packages/vim.rb
@@ -0,0 +1,94 @@
+require 'package'
+
+class Vim < Package
+  description 'Vim is a highly configurable text editor built to make creating and changing any kind of text very efficient.'
+  homepage 'http://www.vim.org/'
+  version '8.2.3892'
+  license 'GPL-2'
+  compatibility 'all'
+  source_url 'https://github.com/vim/vim.git'
+  git_hashtag "v#{@_ver}"
+
+  depends_on 'vim_runtime'
+
+  def self.preflight
+    gvim = `which #{CREW_PREFIX}/bin/gvim 2> /dev/null`.chomp
+    abort "gvim version #{version} already installed.".lightgreen unless gvim.to_s == ''
+  end
+
+  def self.patch
+    abort('Please remove libiconv before building.') if File.exist?("#{CREW_LIB_PREFIX}/libcharset.so")
+    # set the system-wide vimrc path
+    FileUtils.cd('src') do
+      system 'sed', '-i', "s|^.*#define SYS_VIMRC_FILE.*$|#define SYS_VIMRC_FILE \"#{CREW_PREFIX}/etc/vimrc\"|",
+             'feature.h'
+      system 'sed', '-i', "s|^.*#define SYS_GVIMRC_FILE.*$|#define SYS_GVIMRC_FILE \"#{CREW_PREFIX}/etc/gvimrc\"|",
+             'feature.h'
+    end
+  end
+
+  def self.build
+    system '[ -x configure ] || autoreconf -fvi'
+    system "env CFLAGS='-pipe -fno-stack-protector -U_FORTIFY_SOURCE -flto=auto' \
+      CXXFLAGS='-pipe -fno-stack-protector -U_FORTIFY_SOURCE -flto=auto' \
+      LDFLAGS='-fno-stack-protector -U_FORTIFY_SOURCE -flto=auto' \
+      ./configure \
+      #{CREW_OPTIONS} \
+      --localstatedir=#{CREW_PREFIX}/var/lib/vim \
+      --with-features=huge \
+      --with-compiledby='Chromebrew' \
+      --enable-gpm \
+      --enable-acl \
+      --with-x=no \
+      --disable-gui \
+      --enable-multibyte \
+      --enable-cscope \
+      --enable-netbeans \
+      --enable-perlinterp=dynamic \
+      --enable-pythoninterp=dynamic \
+      --enable-python3interp=dynamic \
+      --enable-rubyinterp=dynamic \
+      --enable-luainterp=dynamic \
+      --enable-tclinterp=dynamic \
+      --disable-canberra \
+      --disable-selinux \
+      --disable-nls"
+    system 'make'
+  end
+
+  def self.install
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", "VIMRCLOC=#{CREW_PREFIX}/etc", 'install'
+    FileUtils.ln_s "#{CREW_PREFIX}/bin/vim", "#{CREW_DEST_PREFIX}/bin/vi"
+
+    # these are provided by 'vim_runtime'
+    FileUtils.rm_r "#{CREW_DEST_PREFIX}/share/vim"
+
+    # these are provided by 'xxd_standalone'
+    @deletefiles = %W[#{CREW_DEST_PREFIX}/bin/xxd #{CREW_DEST_MAN_PREFIX}/man1/xxd.1]
+    @deletefiles.each do |f|
+      FileUtils.rm f if  File.exist?(f)
+    end
+
+    # remove desktop and icon files for the terminal package
+    FileUtils.rm_r "#{CREW_DEST_PREFIX}/share/applications"
+    FileUtils.rm_r "#{CREW_DEST_PREFIX}/share/icons"
+  end
+
+  def self.postinstall
+    puts
+    puts "The config files are located in #{CREW_PREFIX}/etc.".lightblue
+    puts 'User-specific configuration should go in ~/.vimrc.'.lightblue
+    puts
+    puts 'If you are upgrading from an earlier version, edit ~/.bashrc'.orange
+    puts "and remove the 'export VIMRUNTIME' and 'export LC_ALL=C' lines.".orange
+    puts
+  end
+
+  def self.check_update
+    @_latest_ver = @_latest_tag = `git ls-remote --tags https://github.com/vim/vim`.scan(/refs\/tags\/(v.*)/).flatten.sort_by do |ver|
+      Gem::Version.new(ver[/^v(.*)/, 1])
+    end[-1]
+
+    return @_latest_ver, @_latest_tag, git_tag: true
+  end
+end
